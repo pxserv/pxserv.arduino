c4f41211d18185c304299eb747bc819a5631874a
Mustafa "pxsty" KÖK <75846829+pxsty0@users.noreply.github.com>
2025-02-11 23:44:28 +0300

    added support for esp8266

--- src/PxServ.h
+++ src/PxServ.h
@@ -4,10 +4,16 @@
 #include <Arduino_JSON.h>
 #include <Arduino.h>
 
+#if defined(ESP32)
 #include <WiFi.h>
 #include <WiFiMulti.h>
 #include <HTTPClient.h>
 #include <WiFiClientSecure.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClientSecureBearSSL.h>
+#endif
 
 class PxServ
 {
